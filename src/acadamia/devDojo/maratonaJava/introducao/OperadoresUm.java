4cbdb30182efb2126a0371556157a70ae4da3cd8
evelin-kashimir <evelin.kashimir@gmail.com>
2022-02-08 20:03:58 -0300

    Fundamentos: Operadores Aritméticos

---
+++ src/acadamia/devDojo/maratonaJava/introducao/OperadoresUm.java
@@ -0,0 +1,23 @@
+package acadamia.devDojo.maratonaJava.introducao;
+
+public class OperadoresUm {
+
+    public static void main(String[] args) {
+
+        //Operadores aritiméticos + - * /
+
+        int num1 = 10, num2 = 20;
+
+        System.out.print("Soma = ");
+        System.out.println(num1+num2);
+
+        System.out.print("Subtração = ");
+        System.out.println(num1-num2);
+
+        System.out.print("Multiplicação = ");
+        System.out.println(num1*num2);
+
+        System.out.print("Divisão = ");
+        System.out.println(num1/num2);
+    }
+}
