a1478e0f0111473aac588143c6ea70c15ee87458
evelin-kashimir <evelin.kashimir@gmail.com>
2022-02-08 18:58:26 -0300

    Fundamentos: Primeiro exercicio

---
+++ src/acadamia/devDojo/maratonaJava/introducao/Exercicio.java
@@ -0,0 +1,22 @@
+package acadamia.devDojo.maratonaJava.introducao;
+
+public class Exercicio {
+
+    //Criar variáveis
+    //Imprimir uma frase com as variáveis criadas
+
+    public static void main(String[] args) {
+
+        String nome = "Evelin Kashimir";
+        String endereco = "Rua Flor da Montanha - Vila Carmela, Guarulhos - SP";
+        int idade = 25;
+        String trabalho = "Analista de Sistemas";
+        double salario = 5200.0;
+        String dataPagamento = "20/02/2022";
+
+        System.out.println("Meu nome é " + nome + ", tenho " + idade + " anos e moro no endereço: " + endereco);
+        System.out.println("Minha profissão é " + trabalho + ", recebo R$" + salario + " no dia: " + dataPagamento);
+
+    }
+
+}
