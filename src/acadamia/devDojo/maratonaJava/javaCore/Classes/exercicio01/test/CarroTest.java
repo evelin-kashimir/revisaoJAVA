ed53fce332e7549e574286e8ab1808e9d6dad553
Evelin Ferraz <evelin.moitinho@capgemini.com>
2022-02-09 19:14:18 -0300

    Orientação a Objetos: Referencia de Objetos

--- src/acadamia/devDojo/maratonaJava/javaCore/Classes/exercicio01/test/CarroTest.java
+++ src/acadamia/devDojo/maratonaJava/javaCore/Classes/exercicio01/test/CarroTest.java
@@ -7,17 +7,21 @@ public class CarroTest {
     public static void main(String[] args) {
 
         Carro carro01 = new Carro();
+        Carro carro02 = new Carro();
+
         carro01.nome = "Gol";
         carro01.modelo = "SUV";
         carro01.ano = 2000;
 
-        System.out.println("Carro: " + carro01.nome + " | Modelo: " + carro01.modelo + " | Ano: " + carro01.ano);
-
-        Carro carro02 = new Carro();
         carro02.nome = "Pálio";
         carro02.modelo = "Esportivo";
         carro02.ano = 2002;
 
+        //referenciando o mesmo objeto
+        carro01 = carro02;
+        carro02 = carro01;
+
+        System.out.println("Carro: " + carro01.nome + " | Modelo: " + carro01.modelo + " | Ano: " + carro01.ano);
         System.out.println("Carro: " + carro02.nome + " | Modelo: " + carro02.modelo + " | Ano: " + carro02.ano);
     }
 }
