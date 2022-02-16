bc74ea5e5d6e7919d1922624a8db930fd8e68837
Evelin Ferraz <evelin.moitinho@capgemini.com>
2022-02-16 17:02:42 -0300

    Orientacao a Objetos: Constantes em Classes e Metodos

--- src/acadamia/devDojo/maratonaJava/javaCore/ModificadorFinal/domain/Carro.java
+++ src/acadamia/devDojo/maratonaJava/javaCore/ModificadorFinal/domain/Carro.java
@@ -1,12 +1,19 @@
 package acadamia.devDojo.maratonaJava.javaCore.ModificadorFinal.domain;
 
-public class Carro {
+//a partir do momento que você torna a sua classe como final, ela não pode ser extendida
+public final class Carro {
     private String nome;
     public static final double VELOCIDADE_LIMITE = 250; //constante
     public final Comprador COMPRADOR = new Comprador(); //objeto constante, não pode ser alterado
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    //esse metodo vai ser sobreescrito na sub classe, porém não pode ser alterado
+    public final void imprime(){
+        System.out.println(this.nome);
+        System.out.println(this.COMPRADOR);
     }
 
     public String getNome() {
