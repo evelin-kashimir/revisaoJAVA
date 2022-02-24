1de6d47a14cf12d4e3832912366729dd2e5ab295
Evelin Ferraz <evelin.moitinho@capgemini.com>
2022-02-23 21:05:57 -0300

    Excecoes: Multi catchs em linha

--- src/acadamia/devDojo/maratonaJava/javaCore/Exception/runtimeTest/RuntimeExceptionTest04.java
+++ src/acadamia/devDojo/maratonaJava/javaCore/Exception/runtimeTest/RuntimeExceptionTest04.java
@@ -1,22 +1,37 @@
 package acadamia.devDojo.maratonaJava.javaCore.Exception.runtimeTest;
 
+import java.io.FileNotFoundException;
+import java.sql.SQLException;
+
 public class RuntimeExceptionTest04 {
     public static void main(String[] args) {
 
         //Multiplas Exceções
         try{
             throw new IllegalArgumentException();
-        }catch (ArrayIndexOutOfBoundsException e){
+        }catch (ArrayIndexOutOfBoundsException | IllegalArgumentException e){
             System.out.println("Dentro do ArrayIndexOutOfBoundsException");
-        }catch (IndexOutOfBoundsException e){
-            System.out.println("Dentro do IndexOutOfBoundsException");
-        }catch (IllegalArgumentException e ){
             System.out.println("Dentro do IllegalArgumentException");
+
+        }catch (IndexOutOfBoundsException e) {
+            System.out.println("Dentro do IndexOutOfBoundsException");
         }
+
         //Exceções sempre devem ser tratadas no final, caso contrario aoutras exceções específicas serão ignoradas
         catch (RuntimeException e){
-
             System.out.println("Dentro do RuntimeException ");
         }
+
+        try{
+            talvezLanceExcecao();
+            //Utilizando pipes
+        }catch (SQLException | FileNotFoundException e ){
+            e.printStackTrace();
+        }
+
+
+    }
+    private static void talvezLanceExcecao() throws SQLException,FileNotFoundException{
+
     }
 }
